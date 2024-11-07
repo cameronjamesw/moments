467a3d5924b1985108bad9099e1c415a84a2f5c6
Cam Wheatley <cameron_2000@live.co.uk>
2024-11-07 15:06:03 +0000

    set up sign in authentication

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -8,29 +8,66 @@ import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+import axios from "axios";
 
 function SignInForm() {
     //   Add your component logic here
+
+    const [signInData, setSignInData] = useState({
+        username: '',
+        password: ''
+    });
+
+    const { username, password } = signInData;
+
+    const [errors, setErrors] = useState({});
+
+    
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setSignInData({
+            ...signInData,
+            [event.target.name]: event.target.value
+        })
+    };
+
+    const handleSubmt = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('/dj-rest-auth/login/', signInData);
+            history.push('/');
+
+        } catch (err) {
+            setErrors(err.response?.data);
+        }
+    };
 
     return (
         <Row className={styles.Row}>
             <Col className="my-auto p-0 p-md-2" md={6}>
                 <Container className={`${appStyles.Content} p-4 `}>
                     <h1 className={styles.Header}>sign in</h1>
-                    {<Form>
+                    {<Form onSubmit={handleSubmt}>
                         <Form.Group controlId="username">
                             <Form.Label className="d-none">Username</Form.Label>
                             <Form.Control
                                 className={styles.Input}
                                 type="text"
                                 placeholder="Username"
-                                name="username" />
+                                name="username"
+                                value={username}
+                                onChange={handleChange} />
                         </Form.Group>
+                        {errors.username?.map((message, idx) => {
+                            <Alert variant="warning" key={idx}>
+                                {message}</Alert>
+                        })}
                         <br />
 
                         <Form.Group controlId="password">
@@ -39,8 +76,14 @@ function SignInForm() {
                                 className={styles.Input}
                                 type="password"
                                 placeholder="Password"
-                                name="password" />
+                                name="password"
+                                value={password}
+                                onChange={handleChange} />
                         </Form.Group>
+                        {errors.password?.map((message, idx) => {
+                            <Alert variant="warning" key={idx}>
+                                {message}</Alert>
+                        })}
                         <br />
 
                         <Button
@@ -48,6 +91,11 @@ function SignInForm() {
                             className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}>
                             Sign In
                         </Button>
+                        {errors.non_field_errors?.map((message, idx) => (
+                            <Alert key={idx} variant="warning" className="mt-3">
+                                {message}
+                            </Alert>
+                        ))}
                     </Form>}
 
                 </Container>
