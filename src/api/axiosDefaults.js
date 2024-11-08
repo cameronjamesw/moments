70f33f140b7f3da6303e4d039459611ffc0b7f43
Cameron Wheatley <cameron_2000@live.co.uk>
2024-11-08 21:52:26 +0000

    complete interceptors lesson

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://cams-api-63fe6b3dbe56.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
