4bf0667458b3e1ee344df015d6cc113f513ff9fd
Cameron Wheatley <cameron_2000@live.co.uk>
2024-11-11 12:35:24 +0000

    update axios

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -17,7 +17,7 @@ import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
 import { useHistory } from "react-router";
-import { axiosReq } from "../../api/axiosDefaults";
+import { axiosReq } from "../../api/axiosDefaults"
 
 function PostCreateForm() {
   const [errors, setErrors] = useState({});
