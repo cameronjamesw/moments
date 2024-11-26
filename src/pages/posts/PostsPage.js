22d0af1d76a52b55bef7dec6c5f035a52238966b
Cameron Wheatley <cameron_2000@live.co.uk>
2024-11-26 17:05:24 +0000

    fix search bar bug

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,83 +1,97 @@
 import React, { useEffect, useState } from "react";
-import Post from './Post';
-import NoResults from '../../assets/no-results.png';
-import Asset from '../../components/Asset'
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
+import Post from "./Post";
+import Asset from "../../components/Asset";
+
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
-import { axiosReq } from "../../api/axiosDefaults"
+import { useLocation } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+
+import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 function PostsPage({ message, filter = "" }) {
-    const [posts, setPosts] = useState({ results: [] });
-    const [hasLoaded, setHasLoaded] = useState(false);
-    const { pathname } = useLocation();
+  const [posts, setPosts] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
 
-    const [query, setQuery] = useState();
+  const [query, setQuery] = useState("");
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
-                setPosts(data);
-                setHasLoaded(true);
-            } catch (err) {
-                console.log(err)
-            }
-        }
-        setHasLoaded(false)
-        const timer = setTimeout(() => {
-            fetchPosts()
-        }, 1000)
-        return () => {
-            clearTimeout(timer);
-        }
-    }, [filter, query, pathname])
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
+        setPosts(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-    return (
-        <Row className="h-100">
-            <Col className="py-2 p-0 p-lg-2" lg={8}>
-                <p>Popular profiles mobile</p>
-                <i className={`fas fa-search ${styles.SearchIcon}`} />
-                <Form
-                    className={styles.SearchBar}
-                    onSubmit={(event) => { event.preventDefault() }}>
-                    <Form.Control
-                        value={query}
-                        onChange={(event) => setQuery(event.target.value)}
-                        type="text"
-                        className="mr-sm-2"
-                        placeholder="Search Posts"/>
-                </Form>
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchPosts();
+    }, 1000);
 
-                {hasLoaded ? (
-                    <>
-                        {posts.results.length ? (
-                            posts.results.map((post) => (
-                                < Post key={post.id} {...post} setPosts={setPosts} />
-                            ))
-                        ) : (
-                            <Container className={appStyles.Content}>
-                                <Asset src={NoResults} message={message} />
-                            </Container>
-                        )}
-                    </>
-                ) : (
-                    <Container className={appStyles.Content}>
-                        < Asset spinner />
-                    </Container>
-                )}
-            </Col>
-            <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-                <p>Popular profiles for desktop</p>
-            </Col>
-        </Row>
-    );
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search posts"
+          />
+        </Form>
+
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              <InfiniteScroll
+                children={posts.results.map((post) => (
+                  <Post key={post.id} {...post} setPosts={setPosts} />
+                ))}
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
+              />
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <p>Popular profiles for desktop</p>
+      </Col>
+    </Row>
+  );
 }
 
 export default PostsPage;
