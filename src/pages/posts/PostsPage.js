a81fb0c1eee205fdbfc7750978e736d4d32e3ccd
Cameron Wheatley <cameron_2000@live.co.uk>
2024-11-28 15:23:35 +0000

    fix inifinte scroll, add profile posts to profile

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -10,10 +10,12 @@ import Asset from "../../components/Asset";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
-import { axiosReq } from "../../api/axiosDefaults"
+import { useLocation } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostsPage({ message, filter = "" }) {
@@ -47,7 +49,7 @@ function PostsPage({ message, filter = "" }) {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p><PopularProfiles mobile/></p>
+        <PopularProfiles mobile />
         <i className={`fas fa-search ${styles.SearchIcon}`} />
         <Form
           className={styles.SearchBar}
@@ -62,29 +64,35 @@ function PostsPage({ message, filter = "" }) {
           />
         </Form>
 
-                {hasLoaded ? (
-                    <>
-                        {posts.results.length ? (
-                            posts.results.map((post) => (
-                                < Post key={post.id} {...post} setPosts={setPosts} />
-                            ))
-                        ) : (
-                            <Container className={appStyles.Content}>
-                                <Asset src={NoResults} message={message} />
-                            </Container>
-                        )}
-                    </>
-                ) : (
-                    <Container className={appStyles.Content}>
-                        < Asset spinner />
-                    </Container>
-                )}
-            </Col>
-            <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-                <p><PopularProfiles/></p>
-            </Col>
-        </Row>
-    );
+        {hasLoaded ? (
+          <>
+            {posts.results.length ? (
+              <InfiniteScroll
+                children={posts.results.map((post) => (
+                  <Post key={post.id} {...post} setPosts={setPosts} />
+                ))}
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
+              />
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
+      </Col>
+      <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+      </Col>
+    </Row>
+  );
 }
 
 export default PostsPage;
