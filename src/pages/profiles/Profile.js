e244343fd0d0bf8705df04a3d5f56f25909befc1
Cameron Wheatley <cameron_2000@live.co.uk>
2024-11-28 15:56:28 +0000

    add follow functionality

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -5,6 +5,7 @@ import { useCurrentUser } from '../../contexts/CurrentUserContexts'
 import { Link } from 'react-router-dom'
 import Avatar from '../../components/Avatar'
 import { Button } from 'react-bootstrap'
+import { useSetProfileData } from '../../contexts/ProfileDataContext'
 
 const Profile = (props) => {
     const { profile, mobile, imageSize = 55 } = props;
@@ -12,6 +13,9 @@ const Profile = (props) => {
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
+
+    const { handleFollow } = useSetProfileData();
+
     return (
         <div
             className={`my-3 d-flex align-items-center ${mobile && "flex-column"} `}>
@@ -30,7 +34,7 @@ const Profile = (props) => {
                             onClick={() => { }}>unfollow</Button>
                     ) : (
                         <Button className={`${btnStyles.Button} ${btnStyles.Black}`}
-                            onClick={() => { }}>follow</Button>
+                            onClick={() => {handleFollow(profile)}}>follow</Button>
                     )
                 )}
 
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -14,3 +14,23 @@ export const fetchMoreData = async (resource, setResource) => {
     }));
   } catch (err) {}
 };
+
+export const followHelper = (profile, clickedProfile, following_id) => {
+    return profile.id === clickedProfile.id
+      ? // This is the profile I clicked on,
+      // update it's followers count and set following id
+      {
+        ...profile,
+        followers_count: profile.followers_count + 1,
+        following_id
+      }
+      : profile.is_owner
+        ? // This is the profile of the logged in user
+        // update its following count
+        {
+          ...profile, following_count: profile.following_count + 1,
+        }
+        : // this is not the profile the user clicked on or the profile
+        // the user owns, so return as normal
+        profile;
+  }
